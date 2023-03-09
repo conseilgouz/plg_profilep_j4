0e18093187132c36e7743628b3e6daf97fde92e3
pmleconte <pascal.leconte@conseilgouz.com>
2023-03-09 13:51:41 +0100

    2.0.3
    Add index.html in profiles folder
    Add script.php to enable plugin

---
+++ script.php
@@ -0,0 +1,128 @@
+<?php
+/**
+* Prev Login Plugin  - Joomla 4.x Plugin 
+* Version			: 2.0.3
+* copyright 		: Copyright (C) 2023 ConseilGouz. All rights reserved.
+* license    		: http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+*/
+// No direct access to this file
+defined('_JEXEC') or die;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Version;
+use Joomla\CMS\Filesystem\File;
+
+class plgUserprofilepInstallerScript
+{
+	private $min_joomla_version      = '4.0.0';
+	private $min_php_version         = '7.4';
+	private $name                    = 'Profile P';
+	private $exttype                 = 'plugin';
+	private $extname                 = 'profilep';
+	private $previous_version        = '';
+	private $dir           = null;
+	private $installerName = 'profilepinstaller';
+	public function __construct()
+	{
+		$this->dir = __DIR__;
+		$this->lang = Factory::getLanguage();
+		$this->lang->load($this->extname);
+	}
+
+    function preflight($type, $parent)
+    {
+		if ( ! $this->passMinimumJoomlaVersion())
+		{
+			$this->uninstallInstaller();
+			return false;
+		}
+
+		if ( ! $this->passMinimumPHPVersion())
+		{
+			$this->uninstallInstaller();
+			return false;
+		}
+    }
+    
+    function postflight($type, $parent)
+    {
+		if (($type=='install') || ($type == 'update')) { // enable plugin
+			$this->postinstall_cleanup();
+		}
+
+		return true;
+    }
+	private function postinstall_cleanup() {
+		$db = Factory::getDbo();
+        $conditions = array(
+            $db->qn('type') . ' = ' . $db->q('plugin'),
+            $db->qn('element') . ' = ' . $db->quote('profilep')
+        );
+        $fields = array($db->qn('enabled') . ' = 1');
+
+        $query = $db->getQuery(true);
+		$query->update($db->quoteName('#__extensions'))->set($fields)->where($conditions);
+		$db->setQuery($query);
+        try {
+	        $db->execute();
+        }
+        catch (RuntimeException $e) {
+            JLog::add('unable to enable plugin profile_prevlogin', JLog::ERROR, 'jerror');
+        }
+	}
+
+	// Check if Joomla version passes minimum requirement
+	private function passMinimumJoomlaVersion()
+	{
+		$j = new Version();
+		$version=$j->getShortVersion(); 
+		if (version_compare($version, $this->min_joomla_version, '<'))
+		{
+			Factory::getApplication()->enqueueMessage(
+				'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
+				'error'
+			);
+
+			return false;
+		}
+
+		return true;
+	}
+
+	// Check if PHP version passes minimum requirement
+	private function passMinimumPHPVersion()
+	{
+
+		if (version_compare(PHP_VERSION, $this->min_php_version, '<'))
+		{
+			Factory::getApplication()->enqueueMessage(
+					'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
+				'error'
+			);
+			return false;
+		}
+
+		return true;
+	}
+	private function uninstallInstaller()
+	{
+		if ( ! JFolder::exists(JPATH_PLUGINS . '/system/' . $this->installerName)) {
+			return;
+		}
+		$this->delete([
+			JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
+			JPATH_PLUGINS . '/system/' . $this->installerName,
+		]);
+		$db = Factory::getDbo();
+		$query = $db->getQuery(true)
+			->delete('#__extensions')
+			->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
+			->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
+			->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+		$db->setQuery($query);
+		$db->execute();
+		Factory::getCache()->clean('_system');
+	}
+	
+}
